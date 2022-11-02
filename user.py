f26d9c57fdabf7d9e52ea87e81fd422f6c3b6467
Axelle COURIOL <axelle.couriol@epita.fr>
2022-11-02 19:54:01 +0100

    [FIX] fix module case sensitive

--- user.py
+++ user.py
@@ -1,8 +1,14 @@
 from PyInquirer import prompt
 user_questions = [
-
+    {
+        'type': 'input',
+        'name': 'first_name',
+        'message': 'What\'s your first name'
+    }
 ]
 
+
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+
+    answers = prompt(questions)
+    return answers
